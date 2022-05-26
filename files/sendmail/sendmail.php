057d833cf5d4887929b15e5a61b8ec9eb9acefbc
hron2017 <andreypbiz@gmail.com>
2022-05-26 21:14:46 +0300

    first commit

---
+++ files/sendmail/sendmail.php
@@ -0,0 +1,67 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+	require 'phpmailer/src/SMTP.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	/*
+	$mail->isSMTP();                                            //Send using SMTP
+	$mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	$mail->Username   = 'user@example.com';                     //SMTP username
+	$mail->Password   = 'secret';                              //SMTP password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+	$mail->Port       = 465;   */                
+	
+
+	//От кого письмо
+	$mail->setFrom('arschool@arschool.online'); // Указать нужный E-mail
+	//Кому отправить
+	$mail->addAddress('arschool.vp@gmail.com'); // Указать нужный E-mail
+
+	//Тема письма
+	$mail->Subject = 'Реєстрація на міні-курс';
+
+	if(trim(!empty($_POST['phone'])) && trim(!empty($_POST['email'])) && trim(!empty($_POST['name']))){
+		$body = "<p><b>Имя пользователя:</b> " . $_POST['name'] . "<br/></p>";
+	        $body.= '<p><b>Телефон:</b> '. $_POST['phone'] .'<br/></p>';
+	        $body.= '<p><b>E-mail пользователя:</b> '. $_POST['email'] .'</p>';
+	}
+
+
+
+	/*
+	//Прикрепить файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//путь загрузки файла
+		$filePath = __DIR__ . "/files/sendmail/attachments/" . $_FILES['image']['name']; 
+		//грузим файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото в приложении</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+	*/
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
